01d33ae9b1c6759130c340b4ce6d893cb9de7aad
Piotrek <252760@edu.p.lodz.pl>
2024-05-16 14:57:06 +0200

    added basic functions and menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "text.h"
+void menu(){
+    printf("1 - Enter Text\n");
+    printf( "2 - Print Text\n");
+    printf( "3 -Text Length\n");
+    printf( "4 -Change To Big Letters\n");
+    printf( "5 -Change To Small Letters\n");
+    printf( "6 -Stop Program\n");
 }
