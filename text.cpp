7f971cdde91ed7cb4788103935a3666dd62f6f0a
Piotrek <252760@edu.p.lodz.pl>
2024-05-16 17:54:10 +0200

    Added functions into text.cpp

--- text.cpp
+++ text.cpp
@@ -1,3 +1,48 @@
 //
 // Created by Piotr on 16.05.2024.
 //
+#include <iostream>
+#include "text.h"
+void entertext(char *text) {
+    printf("Enter Text :\n");
+    fgets(text,99,stdin);
+}
+void printtext(char *text)
+{printf("%s\n",text);}
+void clear(void)
+{
+    while(getchar()!= '\n');
+}
+int count(char *text)
+{
+    int c = 0 ;
+    while(*(text + c))
+    {
+        c++;
+    }
+    c =  c - 1;
+    return c;
+
+}
+void bigLetters(char *string)
+{
+    while (*string)
+    {
+        if ('a' <= *string && *string <= 'z')
+        {
+            *string -= ('a' - 'A');
+        }
+        string++;
+    }
+}
+void smallLetters(char *string)
+{
+    while (*string)
+    {
+        if ('A' <= *string && *string <= 'Z')
+        {
+            *string += ('a' - 'A');
+        }
+        string++;
+    }
+}
