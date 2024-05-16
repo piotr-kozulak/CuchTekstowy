1716156e8fdafc4f522441d877ed2258731a3a57
Piotrek <252760@edu.p.lodz.pl>
2024-05-16 14:54:42 +0200

    added text.cpp and text.h

---
+++ text.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Piotr on 16.05.2024.
+//
---
+++ text.h
@@ -0,0 +1,8 @@
+//
+// Created by Piotr on 16.05.2024.
+//
+
+#ifndef CUCHTEKSTOWY1_TEXT_H
+#define CUCHTEKSTOWY1_TEXT_H
+
+#endif //CUCHTEKSTOWY1_TEXT_H
