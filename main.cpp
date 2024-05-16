86d78d7786008a212169a879b907115a5e722969
Piotrek <252760@edu.p.lodz.pl>
2024-05-16 17:56:56 +0200

    functions in main

--- main.cpp
+++ main.cpp
@@ -8,3 +8,35 @@ void menu(){
     printf( "5 -Change To Small Letters\n");
     printf( "6 -Stop Program\n");
 }
+int main() {
+    char text[100] = " ";
+    int choice = 0 ;
+    do{
+        menu();
+        puts("Make choice\n ");
+        scanf("%d",&choice);
+        clear();
+        switch(choice)
+        {
+            case 1:
+                entertext(text);
+                break;
+            case 2:
+                printtext(text);
+                break;
+            case 3:
+                printf("Text length = %d\n", count(text));
+                break;
+            case 4:
+                bigLetters(text);
+                puts("Change To Big Letters\n");
+                break;
+            case 5:
+                smallLetters(text);
+                puts("Change To Small Letters\n");
+                break;
+            case 6:
+                break;
+        }
+    }while( choice!=6);
+}
