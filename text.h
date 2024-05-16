3a3d30eeab8035e179a29b81eb826b98f5d10f63
Piotrek <252760@edu.p.lodz.pl>
2024-05-16 14:57:06 +0200

    added basic functions and menu

--- text.h
+++ text.h
@@ -4,5 +4,10 @@
 
 #ifndef CUCHTEKSTOWY1_TEXT_H
 #define CUCHTEKSTOWY1_TEXT_H
-
+void entertext(char *text);
+void printtext(char *text);
+void clear(void);
+int count(char *text);
+void bigLetters(char *string);
+void smallLetters(char *string);
 #endif //CUCHTEKSTOWY1_TEXT_H
